1d8f85e2eae3556817427b274b25d01a5717ac09
Zilby <azilby@gmail.com>
2018-10-08 23:48:19 -0400

    progress on grabbing, mostly tooltips

---
+++ Assets/Scripts/Game/Pullable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class for pullable objects. 
+/// </summary>
+public class Pullable : Grabable
+{
+	protected override string Tip
+	{
+		get { return "Press " + GrabInput.ToString() + " To Pull"; }
+	}
+
+	private bool pulling = false;
+
+	protected override void GrabAction()
+	{
+		pulling = !pulling;
+		if (pulling)
+		{
+			ToolTips.TooltipTextEvent(tooltip, "Press " + GrabInput.ToString() + " To Release");
+		}
+		else
+		{
+			ToolTips.TooltipTextEvent(tooltip, Tip);
+		}
+	}
+}
