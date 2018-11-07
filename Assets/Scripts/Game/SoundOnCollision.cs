db5ba53716d3c39094d6e83b3766a8a213aa23c0
Zilby <azilby@gmail.com>
2018-11-07 12:27:21 -0500

    name change

--- Assets/Scripts/Game/SoundOnCollision.cs
+++ Assets/Scripts/Game/SoundOnCollision.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class WaterDroplet : MonoBehaviour
+public class SoundOnCollision : MonoBehaviour
 {
 	public ParticleSystem part;
 
