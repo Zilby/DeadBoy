1c1b3923e2922c0128437797c5a2f1cfe8b79a9c
Zilby <azilby@gmail.com>
2018-11-11 14:13:17 -0500

    added sfx branch scripts

---
+++ Assets/Scripts/Sound/SFXManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+/// <summary>
+/// Manages in-game SFX
+/// </summary>
+public class SFXManager : MonoBehaviour
+{
+	public static SFXManager instance;
+	public enum Sounds
+	{
+		//None = -1,
+		DBFootsteps = 0,
+		DGFootsteps = 1,
+		WaterDrops = 2,
+	}
+	public AudioMixerGroup sfxGroup;
+	[Serializable] public class ClipList : ListWrapper<AudioClip> { }
+	[Serializable] public class ClipDict : SerializableDictionary<Sounds, ClipList> { }
+	public ClipDict clips;
+	// public Dictionary<Sounds, List<AudioClip>> clips;
+	// Make a custom property attribute
+	[InspectorButton("SetUpClips")]
+	public bool SetUpClipKeys;
+	private AudioClip[] lastPlayedClips;
+	private void SetUpClips()
+	{
+		foreach (Sounds s in Enum.GetValues(typeof(Sounds)))
+		{
+			if (!clips.ContainsKey(s))
+			{
+				clips[s] = null;
+			}
+		}
+	}
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+			lastPlayedClips = new AudioClip[Enum.GetValues(typeof(Sounds)).Length];
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+	/// <summary>
+	/// Plays the clip at the given index. 
+	/// </summary>
+	public void PlayClip(Sounds sound, Vector3 location, float volume = 1, float pitch = 1)
+	{
+		ClipList clipList = clips[sound];
+		// Get random clip if list is greater than 1.
+		AudioClip a = clipList[UnityEngine.Random.Range(0, clipList.Count - 1)];
+		if (clipList.Count > 1)
+		{
+			while (a == lastPlayedClips[(int)sound])
+			{
+				a = clipList[UnityEngine.Random.Range(0, clipList.Count - 1)];
+			}
+		}
+		lastPlayedClips[(int)sound] = a;
+		StartCoroutine(PlayClip(a, location, volume, pitch));
+	}
+	/// <summary>
+	/// Plays the given audio clip.
+	/// </summary>
+	public IEnumerator PlayClip(AudioClip a, Vector3 location, float volume = 1, float pitch = 1)
+	{
+		GameObject g = new GameObject(a.ToString(), typeof(AudioSource));
+		yield return null;
+	}
+}
---
+++ Assets/Scripts/Utility/ListWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+ /// <summary>
+/// Used for making lists of lists in the inspector. 
+/// </summary>
+[System.Serializable]
+public class ListWrapper<T>
+{
+	public List<T> list;
+ 	public T this[int key]
+	{
+		get
+		{
+			return list[key];
+		}
+		set
+		{
+			list[key] = value;
+		}
+	}
+ 	public int Count
+	{
+		get
+		{
+			return list.Count;
+		}
+	}
+} 
