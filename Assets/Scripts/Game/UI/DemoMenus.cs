6f09488cb9345d737d323494ef8ba8157b0f3631
Chris Juchem <chrisjuchem15@gmail.com>
2018-11-12 12:39:42 -0500

    fixup demo menus

--- Assets/Scripts/Game/UI/DemoMenus.cs
+++ Assets/Scripts/Game/UI/DemoMenus.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DemoMenus : BaseMenu
 {
+
+	public FadeableUI fadeable;
+
+	public Button play;
+	public Button menu;
+	public Button quit;
+
 	private void Awake()
 	{
-		SongManager.instance.PlaySong(SongManager.Songs.Sewers);
+		if (SongManager.instance != null) {
+			SongManager.instance.PlaySong(SongManager.Songs.Sewers);
+		}
+		
+		if (play != null) {
+			play.onClick.AddListener(delegate { Fader.SceneEvent("DemoLevel"); });
+		}
+		if (menu != null) {
+			menu.onClick.AddListener(delegate { Fader.SceneEvent("DemoSplash"); });
+		}
+		if (quit != null) {
+			quit.onClick.AddListener(delegate { StartCoroutine(Fader.Quit()); } );
+		}
 	}
-
 }
