f584bd3df650cf681c550684ec636e531a9ba9b3
Zilby <azilby@gmail.com>
2018-11-21 07:41:29 -0500

    added convenience class

---
+++ Assets/Scripts/Editor/ManagerCreator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+internal class ManagerCreator
+{
+	[MenuItem("GameObject/Manager/Game", false, 1)]
+	static void CreateGamePrefabs()
+	{
+		CreatePrefabs("Managers/Game");
+	}
+
+	[MenuItem("GameObject/Manager/General", false, 1)]
+	static void CreateGeneralPrefabs()
+	{
+		CreatePrefabs("Managers/General");
+	}
+
+	static void CreatePrefabs(string path) {
+		Object[] prefabs = Resources.LoadAll<Object>(path);
+		foreach (Object o in prefabs)
+		{
+			if (GameObject.Find(o.name) == null)
+			{
+				Object created = PrefabUtility.InstantiatePrefab(o);
+				Undo.RegisterCreatedObjectUndo(created, "Created " + created.name);
+			}
+		}
+	}
+}
