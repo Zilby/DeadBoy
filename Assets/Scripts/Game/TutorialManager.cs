256f7e5fd46c7698bb7ce69aa33157e5b8212f19
Zilby <azilby@gmail.com>
2018-12-04 22:55:24 -0500

    full gameplay loop done

---
+++ Assets/Scripts/Game/TutorialManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+	private void Start()
+	{
+		StartCoroutine(RunTutorial());
+	}
+
+	private IEnumerator RunTutorial() {
+		yield return new WaitForSeconds(1.5f);
+		yield return DBInputManager.instance.SwapTutorial();
+	}
+}
