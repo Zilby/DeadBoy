3c81c95a1ab258696a7fc65c8e51cfac6b9037f4
Zilby <azilby@gmail.com>
2019-01-27 18:47:43 -0500

    level prototype

--- Assets/Scripts/Game/Interactables/Actions/RaiseWaterAction.cs
+++ Assets/Scripts/Game/Interactables/Actions/RaiseWaterAction.cs
@@ -8,15 +8,27 @@ using UnityEngine;
 public class RaiseWaterAction : InteractAction
 {
 	[Header("RaiseWater Fields")]
+	/// <summary>
+	/// The speed.
+	/// </summary>
 	public float speed = 0.01f;
-	public float height = 1;
-	public float width = 1;
-
+	/// <summary>
+	/// The percentage to raise the water. 
+	/// </summary>
+	[Range(-200f, 500f)]
+	public float percentage = 50f;
+	/// <summary>
+	/// The water.
+	/// </summary>
 	public Transform water;
+
 
 	public override IEnumerator Act(PlayerController p)
 	{
 		yield return base.Act(p);
+		percentage /= 100f;
+		float width = percentage * water.localScale.y;
+		float height = (percentage / 2) * water.localScale.y * water.GetComponent<BoxCollider2D>().size.y;
 		while (Mathf.Abs(height) > 0)
 		{
 			float hspeed = height * speed;
@@ -26,6 +38,11 @@ public class RaiseWaterAction : InteractAction
 			height -= hspeed;
 			width -= yspeed;
 			yield return new WaitForFixedUpdate();
+			if (water.localScale.y <= 0)
+			{
+				Destroy(water.gameObject);
+				break;
+			}
 		}
 	}
 }
