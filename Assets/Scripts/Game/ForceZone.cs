4b848a7550b66b82e0a6e984ed25735e386e54ae
Zilby <azilby@gmail.com>
2019-01-31 01:12:22 -0500

    greyboxing progress

---
+++ Assets/Scripts/Game/ForceZone.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// For applying force to players. 
+/// </summary>
+public class ForceZone : MonoBehaviour
+{
+	public Vector2 forceOverTime;
+
+	public List<string> ignorePlayers;
+
+	private List<PlayerController> players;
+
+	private void Awake()
+	{
+		players = new List<PlayerController>();
+	}
+
+	void OnTriggerEnter2D(Collider2D collision) {
+		PlayerController p = collision.gameObject.GetComponent<PlayerController>();
+		if (p && !ignorePlayers.Contains(p.Name)) {
+			players.Add(p);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collision) {
+		PlayerController p = collision.gameObject.GetComponent<PlayerController>();
+		if (p && players.Contains(p))
+		{
+			players.Remove(p);
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		foreach(PlayerController p in players) {
+			p.rBody.AddForce(forceOverTime);
+		}
+	}
+}
---
+++ Assets/Scripts/Game/Interactables/Actions/ParticleSystemToggleAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Action for toggling particle systems emission state. 
+/// </summary>
+public class ParticleSystemToggleAction : InteractAction
+{
+	public List<ParticleSystem> toggled;
+	public float delay = 1.5f;
+
+	public override IEnumerator Act(PlayerController p)
+	{
+		yield return base.Act(p);
+		yield return new WaitForSeconds(1.5f);
+		foreach (ParticleSystem ps in toggled)
+		{
+			var e = ps.emission;
+			e.enabled = !e.enabled;
+		}
+	}
+}
