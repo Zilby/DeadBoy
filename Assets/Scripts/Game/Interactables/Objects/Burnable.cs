8c1c49c6c8cfd26f14f2925ae1d739d9d2f4d6d2
Chris Juchem <chrisjuchem15@gmail.com>
2019-02-28 09:37:14 -0500

    FK Prototype tweaks

--- Assets/Scripts/Game/Interactables/Objects/Burnable.cs
+++ Assets/Scripts/Game/Interactables/Objects/Burnable.cs
@@ -7,9 +7,11 @@ public class Burnable : Interactable
     /// <summary>
 	/// Checks if the given player is valid. 
 	/// </summary>
-	protected virtual bool PlayerCheck(PlayerController p)
+	protected override bool PlayerCheck(PlayerController p)
 	{
-        return p is FirekidController && base.PlayerCheck(p);
+        Debug.Log((p is FirekidController));
+        Debug.Log(base.PlayerCheck(p));
+        return (p is FirekidController) && base.PlayerCheck(p);
     }
 
     protected override string Tip(PlayerController p)
