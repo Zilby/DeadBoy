78ef928175ae5d49e77e7f9abc6e399af775ed46
Zilby <azilby@gmail.com>
2019-03-08 18:24:36 -0500

    added dialogue triggers

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CutsceneTrigger : MonoBehaviour
+public class DialogueTrigger : MonoBehaviour
 {
-	public Cutscene cutscene;
+    [StringInList(typeof(DialogueManager), "GetDialogueList")]
+	public string name;
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
 		{
-			DialogueManager.instance.StartCoroutine(cutscene.ExecuteCutscene());
+			DialogueManager.instance.StartCoroutine(DialogueManager.instance.BeginDialogue(name));
 			Destroy(gameObject);
 		}
 	}
