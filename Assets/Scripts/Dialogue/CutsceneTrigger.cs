3751d9ec045d40c2584fa54766a930482a629c19
Chris Juchem <chrisjuchem15@gmail.com>
2019-03-08 15:03:55 -0500

    rough sewer 1 cutscene implementation

---
+++ Assets/Scripts/Dialogue/CutsceneTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneTrigger : MonoBehaviour
+{
+    public Cutscene cutscene;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            StartCoroutine(cutscene.ExecuteCutscene());
+            Destroy(gameObject);
+        }
+    }
+}
