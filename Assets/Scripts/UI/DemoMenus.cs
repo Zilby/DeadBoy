55a7926a08ff45a289647f441e178755fe6cd6d6
Zilby <azilby@gmail.com>
2019-03-20 20:26:53 -0400

    Menu improvements, level select

--- Assets/Scripts/UI/DemoMenus.cs
+++ Assets/Scripts/UI/DemoMenus.cs
@@ -8,27 +8,44 @@ public class DemoMenus : MonoBehaviour
 	[StringInList(typeof(SongManager), "GetClipList")]
 	public string song;
 
-	public FadeableUI fadeable;
-
 	public Button play;
 	public Button menu;
 	public Button quit;
+	public Button levelSelect;
+	public Button settings;
+	public List<Button> mainMenu;
+
+	public Fadeable mainf;
+	public Fadeable settingsf;
+	public Fadeable levelSelectf;
 
 	private void Start()
 	{
-		if (SongManager.instance != null) 
+		if (SongManager.instance != null)
 		{
 			SongManager.instance.PlaySong(song);
 		}
-		
-		if (play != null) {
-			play.onClick.AddListener(delegate { Fader.SceneEvent("Desert1"); });
-		}
-		if (menu != null) {
-			menu.onClick.AddListener(delegate { Fader.SceneEvent("DemoStart"); });
-		}
-		if (quit != null) {
-			quit.onClick.AddListener(delegate { StartCoroutine(Fader.Quit()); } );
+
+		play?.onClick.AddListener(delegate { Fader.SceneEvent("Desert1"); });
+		menu?.onClick.AddListener(delegate { Fader.SceneEvent("DemoStart"); });
+		quit?.onClick.AddListener(delegate { StartCoroutine(Fader.Quit()); });
+		settings?.onClick.AddListener(delegate { mainf.SelfFadeOut(); settingsf.SelfFadeIn(); });
+		levelSelect?.onClick.AddListener(delegate { mainf.SelfFadeOut(); levelSelectf.SelfFadeIn(); });
+
+		foreach (Button b in mainMenu)
+		{
+			b.onClick.AddListener(delegate
+			{
+				if (settingsf.IsVisible)
+				{
+					settingsf.SelfFadeOut();
+				}
+				if (levelSelectf.IsVisible)
+				{
+					levelSelectf.SelfFadeOut();
+				}
+				mainf.SelfFadeIn();
+			});
 		}
 	}
 }
---
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour
+{
+	private void Awake()
+	{
+		Transform areas = transform.Find("Areas");
+		foreach(Transform area in areas)
+		{
+			foreach(Transform level in area)
+			{
+				level.GetComponent<Button>().onClick.AddListener(delegate { Fader.SceneEvent(area.name + level.name); });
+			}
+		}
+	}
+}
