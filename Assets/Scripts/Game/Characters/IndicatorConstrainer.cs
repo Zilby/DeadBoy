b0dcdd8dca4ba327040d889f06595bb1435958e2
Zilby <azilby@gmail.com>
2019-03-24 22:31:55 -0400

    Indicator constraints

---
+++ Assets/Scripts/Game/Characters/IndicatorConstrainer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorConstrainer : MonoBehaviour
+{
+	private float defaultY;
+
+	private const float RANGE = 4.5f;
+
+	private void Awake()
+	{
+		defaultY = transform.localPosition.y;
+	}
+
+	private void Update()
+	{
+		SetPosition();
+	}
+
+	private void FixedUpdate()
+	{
+		SetPosition();
+	}
+
+	private void SetPosition()
+	{
+		if (transform.position.y > Camera.main.transform.position.y + RANGE ||
+			transform.position.y < Camera.main.transform.position.y - RANGE)
+		{
+			transform.position = transform.position.Y(Mathf.Clamp(transform.position.y,
+				Camera.main.transform.position.y - RANGE, Camera.main.transform.position.y + RANGE));
+		}
+		else
+		{
+			transform.localPosition = transform.localPosition.Y(defaultY);
+		}
+	}
+}
