2170b057803975d2fb0041f1a3efbbd87e88dee9
Chris Juchem <chrisjuchem15@gmail.com>
2019-07-23 19:21:34 -0400

    tweaks and missing files

---
+++ Assets/Scripts/Game/Interactables/Actions/BreakJointAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Action for breaking a joint. 
+/// </summary>
+public class BreakJointAction : InteractAction
+{
+	public Joint2D joint;
+
+	public override IEnumerator Act(PlayerController p)
+	{
+		yield return base.Act(p);
+		Destroy(joint);
+        yield return null;
+	}
+}
---
+++ Assets/Scripts/Game/Interactables/Objects/Kickable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kickable : Interactable
+{
+	protected override string Tip(PlayerController p)
+	{
+		return "Press " + DBInputManager.GetInputName(p, InteractInput) + " To Kick";
+	}
+
+	protected override void InteractAction(PlayerController p)
+	{
+        p.invertDirection = true;
+		base.InteractAction(p);
+		p.Kick(transform, delegate
+		{
+			SelfDestruct();
+		});
+		EndInteraction();
+	}
+
+    protected virtual void OnTriggerStay2D(Collider2D collision)
+	{
+		PlayerController p = collision.attachedRigidbody?.GetComponent<PlayerController>();
+		if (p != null && PlayerCheck(p))
+		{
+            if (p.Grounded) {
+                EndInteraction();
+            }
+        }
+        
+    }
+}
